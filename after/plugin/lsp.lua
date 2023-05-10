48b0a7564776c8722cd1976f285e07aa9d23aa86
vikksr <contact@vikasr.com>
2023-05-10 20:51:26 +0530

    manage lsp-config directly instead of through lsp-zero

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,106 +1,56 @@
+-- https://dev.to/vonheikemen/make-lsp-zeronvim-coexists-with-other-plugins-instead-of-controlling-them-2i80
 local lsp_zero = require('lsp-zero')
+local lsp_config = require('lspconfig')
+local mason = require('mason')
+local mason_lspconfig = require("mason-lspconfig")
 local cmp = require('cmp')
-local lsp_config = require("lspconfig")
-local lsp_format = require("lsp-format")
 
--- Load snippets from friendly-snippets into nvim-cmp
+-- Configure LSP Servers
+lsp_zero.extend_lspconfig()
+mason.setup()
+mason_lspconfig.setup()
+
+-- Use for loop and call setup or use setup_handlers and call setup
+-- local get_servers = mason_lspconfig.get_installed_servers
+-- for _, server_name in ipairs(get_servers()) do
+  -- lsp_config[server_name].setup({})
+-- end
+
+mason_lspconfig.setup_handlers({
+  function(server_name)
+    lsp_config[server_name].setup({})
+  end,
+  -- language specific configutaion can be added as below
+  -- ['tsserver'] = function()
+    --require('lspconfig').tsserver.setup({
+      --on_attach = function(client, bufnr)
+        --print('hello tsserver')
+      --end,
+      --settings = {
+        --completions = {
+          --completeFunctionCalls = true
+        --}
+      --}
+    --})
+  --end
+})
+
+-- Diagnostic Config
+lsp_zero.set_sign_icons()
+vim.diagnostic.config(lsp_zero.defaults.diagnostics({}))
+
+-- Snippet Config
+require('luasnip').config.set_config({
+  region_check_events = 'InsertEnter',
+  delete_check_events = 'InsertLeave'
+})
+
 require('luasnip.loaders.from_vscode').lazy_load()
--- if instead of friendly-snippets, we want to use `honza/vim-snippets`, then
--- require('luasnip.loaders.from_snipmate').lazy_load()
 
-local lsp = lsp_zero.preset({})
 
--- (Optional) Configure lua language server for neovim
--- Else nvim/*.lua files keep showing lsp errors
-lsp_config.lua_ls.setup(lsp.nvim_lua_ls())
+-- AutoCompletion
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 
-lsp.on_attach(function(client, bufnr)
-  -- if one formatter per file is used, order is not guranteed is used with multiple
-  -- formatters for a single file.
-  -- lsp.default_keymaps({buffer = bufnr})
-  local opts = { buffer = bufnr, remap = false }
-  local allow_format = { 'lua_ls', }
+local cmp_config = lsp_zero.defaults.cmp_config({})
 
-  vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set('n', 'td', function() vim.lsp.buf.type_definition() end, opts)
-  vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration() end, opts)
-  vim.keymap.set('n', 'gi', function() vim.lsp.buf.implementation() end, opts)
-  vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set('n', '<C-k>', function() vim.lsp.buf.signature_help() end, opts)
-  vim.keymap.set('n', '<leader>cg', function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set('n', '<leader>ca', function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set('v', '<leader>ca', function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set('n', '<leader>crn', function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set('n', '<leader>wa', function() vim.lsp.buf.add_workspace_folder() end, opts)
-  vim.keymap.set('n', '<leader>wr', function() vim.lsp.buf.remove_workspace_folder() end, opts)
-  vim.keymap.set('n', '<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, opts)
-  vim.keymap.set('n', '<leader>ws', function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>cf", function()
-    -- vim.lsp.buf.format()
-    vim.lsp.buf.format({ async = true, timeout_ms = 3000 })
-  end)
-
-  if vim.tbl_contains(allow_format, client.name) then
-    lsp_format.on_attach(client)
-  end
-end)
-
-lsp.ensure_installed({
-  'solargraph',
-  'lua_ls',
-  'tailwindcss',
-  'emmet_ls'
-})
-
-lsp.format_on_save({
-  format_opts = {
-    timeout_ms = 10000,
-  },
-  servers = {
-    ['lua_ls'] = { 'lua' },
-    ['solargraph'] = { 'ruby' },
-    ['emmet_ls'] = { 'eruby' }
-  }
-})
-
-lsp.setup()
-
--- Make sure you setup `cmp` after lsp-zero
-
-local cmp_action = lsp_zero.cmp_action()
-
-cmp.setup({
-  preselect = 'item',
-  completion = {
-    completeopt = 'menu,menuone,noinsert'
-  },
-  sources = {
-    { name = 'path' },
-    { name = 'nvim_lsp' },
-    { name = 'nvim_lua' },
-    { name = 'buffer',  keyword_length = 3 },
-    { name = 'luasnip', keyword_length = 2 },
-  },
-  mapping = {
-    -- ['<Tab>'] = cmp_action.tab_complete(),
-    -- ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
-    -- ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-    -- ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-    -- ['<CR>'] = cmp.mapping.confirm({select = false}),
-    ['<Tab>'] = cmp_action.luasnip_supertab(),
-    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
-  },
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  },
-  formatting = {
-    -- changing the order of fields so the icon is the first
-    fields = { 'menu', 'abbr', 'kind' },
-  },
-})
+cmp.setup(cmp_config)
