a7f1ffac1ac18e514f7fb284a5c0772ade12d23d
vikksr <contact@vikasr.com>
2023-05-12 01:57:25 +0530

    some default keymaps for easy access

--- after/plugin/commentary.lua
+++ after/plugin/commentary.lua
@@ -1,2 +1,9 @@
+-- Default keymaps:
+-- gc
+-- gc2j, gc5j, gck, gcj, ...gc[num][movement]
+-- gcc
+-- gcap
+-- :g/TODO/Commentary
+
 vim.keymap.set("n", "<leader>/", "<Plug>CommentaryLine<CR>")
 vim.keymap.set("v", "<leader>/", "<Plug>Commentary<CR>")
