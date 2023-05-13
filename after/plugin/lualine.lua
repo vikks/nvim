1e13dd356004a00afbefaf2335ca8e70b01e7497
vikksr <contact@vikasr.com>
2023-05-13 16:50:12 +0530

    Lualine: show a list of LSPs rather than a single lsp.

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,51 +1,61 @@
 -- Override 'encoding': Don't display if encoding is UTF-8.
 local encoding = function()
-    local ret, _ = (vim.bo.fenc or vim.go.enc):gsub("^utf%-8$", "")
-     return ret
+  local ret, _ = (vim.bo.fenc or vim.go.enc):gsub("^utf%-8$", "")
+  return ret
 end
 
 -- fileformat: Don't display if &ff is unix.
 local fileformat = function()
-    local ret, _ = vim.bo.fileformat:gsub("^unix$", "")
-    return ret
-  end
+  local ret, _ = vim.bo.fileformat:gsub("^unix$", "")
+  return ret
+end
 
-local lsp =  function()
+
+local lsp = {
+  function()
     local msg = 'No Active Lsp'
     local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
     local clients = vim.lsp.get_active_clients()
     if next(clients) == nil then
       return msg
     end
+    local X = ""
     for _, client in ipairs(clients) do
       local filetypes = client.config.filetypes
       if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-        return client.name
+        X = client.name .. "," .. X
       end
     end
-    return msg
-  end
+    if not (X == "") then
+      return X:sub(1, -2)
+    else
+      return msg
+    end
+  end,
+  icon = ' LSP:',
+  color = { fg = '#ffffff', gui = 'bold' }
+}
 
 require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {lsp, encoding, fileformat, 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = { 'filename' },
+    lualine_x = { lsp, encoding, fileformat, 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
+    lualine_c = { 'filename' },
+    lualine_x = { 'location' },
     lualine_y = {},
     lualine_z = {}
   },
